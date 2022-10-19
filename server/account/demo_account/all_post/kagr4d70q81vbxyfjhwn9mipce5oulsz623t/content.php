eab4db37833d6b8028a03897e5c3a945d9abca52
sunstation12 <110587752+sunstation12@users.noreply.github.com>
2022-10-19 18:15:44 +0700

    Add files via upload

---
+++ server/account/demo_account/all_post/kagr4d70q81vbxyfjhwn9mipce5oulsz623t/content.php
@@ -0,0 +1,38 @@
+<p>Origami, hay còn gọi là xếp giấy, có thể coi là một trong những bộ môn nghệ thuật nổi tiếng của đất nước Nhật Bản. Tuy nhiên, lại không nhiều người biết đến kirigami, một môn nghệ thuật từ cắt giấy đa chiều và biến hóa đến kinh ngạc của xứ sở hoa anh đào. Và mới đây, nhà thiết kế Haruki Nakamura đã cho chúng ta thấy được sự ảo diệu trong các tác phẩm cắt giấy tạo hình kirigami.
+<br><br>
+Nếu như origami truyền thống không cho phép người chơi được dùng gì ngoài giấy thì kirigami lại hoàn toàn khác biệt. Người chơi kirigami có thể thoải mái sử dụng kéo cắt, hồ dán để tạo hình, cũng chính vì thế mà tính sáng tạo của kirigami cao hơn hẳn. Nhờ vào điều này, nhà thiết kế Nakamura đã có thể tạo ra các tác phẩm gấp giấy biến đổi hình dạng độc đáo có một không hai.</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;" src="https://genk.mediacdn.vn/thumb_w/640/2018/12/2/photo-1-1543693537310641046256.jpg">
+</div>
+<br>
+<p>Thế nhưng với sự sáng tạo của mình, tác giả Nakamura đã tạo ra các tác phẩm có thể biến hình khi chạm vào.</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://genk.mediacdn.vn/2018/12/2/or2-1499092650893-15436936867901675390178.gif">
+</div>
+<br>
+<p>Nakamura bật mí rằng phía bên trong các tác phẩm kirigami có một bộ phận gọi là karakuri giúp chúng có thể thay đổi hình dạng.</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://genk.mediacdn.vn/thumb_w/640/2018/12/2/photo-1-1543693541936969737512.jpg">
+</div>
+<br>
+<p>Chỉ cần nhấn hoặc thả nhẹ nhàng là chúng có thể "biến hình".</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://genk.mediacdn.vn/2018/12/2/kirigami-2-1499090392543-15436936867931712611623.gif">
+</div>
+<br>
+<p>Các tác phẩm của Haruki Nakamura rất được giới xếp giấy yêu thích và hâm mộ nhờ vào tính sáng tạo độc đáo.</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://genk.mediacdn.vn/2018/12/2/or1-1499092650885-15436936867941646289212.gif">
+</div>
+<br>
+<p>Các tác phẩm của ông cũng khá rẻ, chỉ có giá từ 500 cho tới 1000 yen (khoảng 100 đến 200 nghìn VND). Tuy nhiên, đáng tiếc rằng chúng chỉ có thể mua được nếu như bạn ở Nhật mà thôi.</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://genk.mediacdn.vn/2018/12/2/kirigami-1-1499090382709-1543693686798296094010.gif">
+</div>
+<br>
---
+++ server/account/demo_account/all_post/kagr4d70q81vbxyfjhwn9mipce5oulsz623t/describe.php
@@ -0,0 +1,1 @@
+<p>Không như tác phẩm xếp giấy khác, những con thú này có thể thay đổi hình dáng khi người chơi nhẹ nhàng nhấn hay thả chúng xuống.</p>
