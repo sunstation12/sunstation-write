e98b4ba8036eac51d7856f426c313e8fae611f6f
sunstation12 <110587752+sunstation12@users.noreply.github.com>
2022-10-19 18:10:06 +0700

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<header>
+    <div id="header_navigation_icon">
+        <i id="header_menu_icon" class="fa-solid fa-bars"></i>
+    </div>
+    <div id="header_web_name">
+        Sun Station Write
+    </div>
+    <div id="header_navigation_div">
+        <div class="header_items">
+            <a class="header_items_link" href="/sunstation_write/web/home">
+                Trang Chủ
+            </a>
+        </div>
+        <div class="header_items">
+            <a class="header_items_link" href="/sunstation_write/web/post">
+                Bài viết
+            <a>
+        </div>
+        <div class="header_items">
+            <a class="header_items_link" href="/sunstation_write/web/sign_in">
+                Đăng nhập
+            <a>
+        </div>
+        <div class="header_items">
+            <a class="header_items_link" href="/sunstation_write/web/about">
+                Về Chúng Tôi
+            <a>
+        </div>
+    </div>
+</header>
---
+++ includes/html_head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Icon Setting -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
+
+    <!-- Shared CSS -->
+    <link rel="stylesheet" href="/sunstation_write/setting/shared_file/style.css">
+    <link rel="stylesheet" href="/sunstation_write/setting/shared_file/tag.css">
+
+    <!-- Shared Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Font Setting -->
+    <link href="https://fonts.googleapis.com/css2?family=Alata&family=Bahianita&family=Barlow+Condensed&family=Dosis:wght@500&family=Exo+2&family=Fira+Sans&family=Itim&family=Kanit&family=Mitr&family=Monda&family=Patrick+Hand&family=Roboto&family=Signika&family=Signika+Negative&family=Varela+Round&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
