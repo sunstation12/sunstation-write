b7f6537679e8820e0a364aa463c021131c99fe3a
sunstation12 <110587752+sunstation12@users.noreply.github.com>
2022-10-19 18:12:24 +0700

    Create content.php

---
+++ server/account/demo_account/all_post/d1e76milg9rn3zcvfuwk84xbq2t50yasphoj/content.php
@@ -0,0 +1,48 @@
+<p><p>
+Mì ăn liền là một mặt hàng phổ biến ở nhiều quốc gia, đặc biệt là tại các nước châu Á. Với lợi thể giá rẻ, tiện lợi và có hương vị thơm ngon khó cưỡng, mì tôm đặc biệt được người trẻ ưa thích.
+<br><br>
+Cùng sự phát triển của xã hội, giờ đây, các loại mì đã trở nên vô cùng đa dạng từ hình thức, hương vị. Tuy nhiên, bạn đã bao giờ thắc mắc khởi đầu của món ăn huyền thoại này là như thế nào chưa? Nó từ đâu đến? Nó ẩn chứa những gì? Để hiểu thêm về món ăn rất đỗi quen thuộc này, hãy khám phá 5 sự thật dưới đây về món ăn này nhé, đảm bảo chưa chắc bạn đã biết hết đâu nha!
+</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://gamek.mediacdn.vn/133514250583805952/2022/8/28/photo-1-1661676066581924802699.jpg">
+</div>
+<br>
+<p><span>1. "Cha đẻ" của mì tôm ăn liền?</span></p>
+<br>
+<p>
+Vào khoảng thời gian nước Nhật còn gặp nhiều khó khăn, người đàn ông tên Momofuku Ando đã nảy lên ý tưởng sản xuất mì ăn liền khi ông nhìn thấy một hàng người xếp hàng dài kiên nhẫn chờ đợi một tô mì ramen.
+<br><br>
+Do vậy, ông muốn tạo ra một sản phẩm ngon, rẻ và dễ chế biến trong thời gian ngắn. Vào năm 1958, ông lần đầu tiên giới thiệu món mì tôm và sau đó là mì cốc vào năm 1971.
+</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://gamek.mediacdn.vn/thumb_w/640/133514250583805952/2022/8/28/photo-2-1661676071399358632514.jpg">
+</div>
+<br>
+<p><span>2. Mì tôm từng được coi là một món ăn xa xỉ</span></p>
+<br>
+<p>Trước khi trở nên phổ biến với giá thành rẻ như hiện tại, mì tôm đã từng được coi là một "xa xỉ phẩm". Nguyên nhân là do khi mới được ra mắt nó có giá khoảng 35 yên, đắt hơn 6 lần so với giá thành của mì udon tươi mua từ những cửa hàng tạp hoá thời bấy giờ.</p>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://gamek.mediacdn.vn/133514250583805952/2022/8/28/photo-3-1661676071459136533173.jpg">
+</div>
+<br>
+<p><span>3. Người Nhật Bản cho rằng mì là một trong những phát minh vĩ đại nhất</span>
+<br><br>
+Cho đến thời điểm hiện tại, vị trí của mì ăn liền vẫn khó có thể thay thế.
+</p>
+<br>
+<p><span>4. Mì ăn liền là loại mì đầu tiên được ăn ngoài vũ trụ</span></p>
+<br>
+<p>Được phát minh bởi Momofuku Ando vào năm 2005, "Space Ram" là một loại mì được đóng gói chân không với sợi mì có kết cấu nhỏ hơn và nước dùng đặc hơn. Thức ăn không gian này được phát minh cho chuyến du hành của phi hành gia Nhật Bản Soichi Noguchi trong tàu con thoi Discovery.</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px;" src="https://gamek.mediacdn.vn/133514250583805952/2022/8/28/photo-5-1661676071607821595394.jpg">
+</div>
+<br>
+<p><span>5. Chiều dài sợi mì bên trong một gói mì ăn liền là 51 mét</span></p>
+<br>
+<p>Được thiết kế sao cho nằm gọn gàng trong bao bì với những vắt mì được nén lại với hình dáng uốn lượn, thật khó có thể tin rằng sợi mì gói lại có chiều dài lên tới 51 mét nếu duỗi sợi mì theo đường thẳng. Đây cũng là chiều dài tương đương với hai sân bóng rổ tiêu chuẩn. Thật là bất ngờ phải không!
+<br><br>
+Hoá ra sợi mì lại có chiều dài khủng đến thế!
+</p></p>
