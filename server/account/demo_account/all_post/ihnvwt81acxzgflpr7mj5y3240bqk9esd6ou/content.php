010a3fab4cee46f414abf73e13dcc286467e75d5
sunstation12 <110587752+sunstation12@users.noreply.github.com>
2022-10-19 18:14:33 +0700

    Create content.php

---
+++ server/account/demo_account/all_post/ihnvwt81acxzgflpr7mj5y3240bqk9esd6ou/content.php
@@ -0,0 +1,19 @@
+<p>
+Cụ thể, một vài chiếc máy tính cũ chạy Windows XP sẽ gặp hư hỏng nghiêm trọng liên quan đến ổ cứng nếu người dùng bật MV ca nhạc của “Rhythm Nation” của nữ minh tinh Janet Jackson.
+</p>
+<br>
+<div style="text-align:center">
+<img style="height:400px;border-radius:20px" src="https://gamek.mediacdn.vn/thumb_w/640/133514250583805952/2022/8/28/photo-1-16616581493981590772411.jpg">
+</div>
+<br>
+<p>
+Ông Raymond Chen, kỹ sư phần mềm kỳ cựu của Microsoft cho biết một công ty sản xuất máy tính đã phát hiện vài sản phẩm của họ đã hỏng nặng khi đang phát bài hát này. Sự hỏng hóc kỳ lạ còn lan sang những thiết bị gần đó. Tuy nhiên, hiện tượng hi hữu này không chỉ xuất hiện trên máy tính công ty kể trên.
+<br><br>
+Theo ông Chen, nguyên nhân của hiện tượng này khả năng cao đến từ một đoạn âm thanh trong bài hát và nó có khả năng cộng hưởng với các ổ cứng HDD có tốc độ quay 5400 vòng/phút trên laptop. Điều này đồng nghĩa với việc khi người dùng mở nhạc, sóng âm phát ra từ loa máy tính sẽ “phá hủy” ổ cứng, theo đúng nghĩa đen.
+<br><br>
+Để giải quyết vấn đề, nhà sản xuất giấu tên đã thêm một bộ lọc âm thanh để nhận diện và loại bỏ tần số của sóng âm cộng hưởng kể trên ra khỏi MV “Rhythm Nation”. Ngoài ra nếu không muốn máy tính của mình gặp trục trặc người dùng chỉ cần bật những bài hát khác, không chứa thanh âm có tần số tương tự.
+<br><br>
+Tuy nhiên, theo chuyên gia, hiện vẫn chưa xác định rõ lỗi này có gây ảnh hưởng trên diện rộng hay không vì chưa hề xuất hiện bất kỳ video nào ghi lại cảnh các máy tính bị hỏng khi đang phát bài hát “Rhythm Nation”.
+<br><br>
+Nhưng người dùng cũng không nên quá lo lắng vì hiện tại những ổ cứng HDD với tốc độ quay thấp đã không còn quá phổ biến trên thị trường hoặc bị thay thế bằng các ổ SSD tiên tiến và bền bỉ hơn. Tuy nhiên trường hợp hi hữu kể trên đã cho thấy các hiện tượng vật lý cũng có thể ảnh hưởng quan trọng đến các thiết bị điện tử tinh vi, thậm chí làm chúng hư hỏng hoàn toàn.
+</p>
