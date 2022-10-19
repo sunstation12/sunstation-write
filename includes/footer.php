e2b49003b60e978f68d7214b979f1174a541add8
sunstation12 <110587752+sunstation12@users.noreply.github.com>
2022-10-19 18:08:55 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,99 @@
+<footer>
+    <div id="sunstation">
+        <a href="https://www.youtube.com/channel/UCVOoUEQaFXVblY6wF1vg_WQ" class="sunstation_group" target="_blank">
+            Youtube Sun Station
+        </a>
+        <a href="https://www.youtube.com/channel/UCyJnpTsqMlCE1cGlRgrPr3w" class="sunstation_group" target="_blank">
+            Youtube Sun Station Film
+        </a>
+        <a href="https://www.youtube.com/channel/UCOq9tiWse3VTx2794sPiiIw" class="sunstation_group" target="_blank">
+            Youtube Big Game Thực Tế
+        </a>
+        <a href="https://tabby-eager-quark.glitch.me" class="sunstation_group" target="_blank">
+            Website Sun Station
+        </a>
+        <a href="https://sunstation-film.glitch.me" class="sunstation_group" target="_blank">
+            Website Sun Station Film
+        </a>
+        <a href="https://plant-jolly-house.glitch.me" class="sunstation_group" target="_blank">
+            Website Sun Station Project
+        </a>
+    </div>
+    <div id="footer_content">
+        <div class="footer_div" id="about_website">
+            <div id="website_name">
+                Sun Station Write
+            </div>
+            <div class="website_about footer_add_content">
+                Trang Web giúp bạn viết Truyện, Ghi Chú, Bài viết, Đoạn văn, Những dòng chữ quan trọng,... tất cả dữ liệu của bạn đều được bảo mật và mã hóa.
+            </div>
+            <div class="website_about footer_add_content">
+                Bạn có thể lựa chọn chế độ công khai, riêng tư hoặc chia sẻ cho bài viết của mình.
+            </div>
+            <div class="website_about footer_add_content">
+                Giới thiệu:
+                <a class="footer-web_link link_about_web" href="/sunstation_write/web/about">
+                    Về Sun Station
+                </a>
+            </div>
+        </div>
+        <div class="footer_div" id="feature">
+            <div class="footer_title">Tính năng</div>
+            <div class="footer_add_content">- Lưu trữ Bài viết không giới hạn;</div>
+            <div class="footer_add_content">- Tùy chỉnh Giao diện Tài khoản;</div>
+            <div class="footer_add_content">- Khôi phục Bài viết đã xóa sau 1 thời gian dài;...</div>
+            <!-- <div class="footer_add_content">- Thêm vào Bài Viết bất kì nội dung nào bạn muốn (Khung nhập, bảng,...)</div> -->
+            <div class="footer_add_content">- Công cụ kèm theo Trình tạo Bài viết trực quan, dễ sử dụng;</div>
+            <div class="footer_add_content">- Chế độ bảo vệ tăng cường, chống copy bài viết (Chưa chính thức).</div>
+        </div>
+        <div class="footer_div" id="contact_and_feature">
+            <div id="contact">
+                <div class="footer_title">Liên hệ</div>
+                <div class="footer_add_content" id="phone_number">
+                    Điện thoại:
+                    <a href="tel:0857.948.688">0857948688</a>
+                </div>
+                <div class="footer_add_content" id="email_contact">
+                    Email:
+                    <a href="mailto:sunstationor@gmail.com">sunstationor@gmail.com</a>
+                </div>
+                <div class="footer_add_content" id="contact_icon">
+                    <a href="https://www.youtube.com/channel/UCVOoUEQaFXVblY6wF1vg_WQ" target="_blank">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                    <a href="https://www.tiktok.com/@tiktoksun15" target="_blank">
+                        <i class="fa-brands fa-tiktok"></i>
+                    </a>
+                </div>
+            </div>
+            <div id="display">
+                <div class="footer_title">Giao diện</div>
+                <div class="footer_add_content" id="phone_number">
+                    Bảng màu:
+                    <a href="https://color.adobe.com/Sun-Station-Write-Color-Default-color-theme-20513897/" target="_blank">https://color.adobe.com</a>
+                </div>
+                <div class="footer_add_content" id="email_contact">
+                    Font:
+                    <a href="https://fonts.googleapis.com/css2?family=Alata&family=Bahianita&family=Barlow+Condensed&family=Dosis:wght@500&family=Exo+2&family=Fira+Sans&family=Itim&family=Kanit&family=Mitr&family=Monda&family=Patrick+Hand&family=Roboto&family=Signika&family=Signika+Negative&family=Varela+Round&family=Yanone+Kaffeesatz&display=swap" target="_blank">https://fonts.googleapis.com</a>
+                </div>
+            </div>
+        </div>
+            
+        <div id="brand">
+            <i class="fa-solid fa-trademark"></i>
+            8/8/2022.
+            1 sản phẩm của <span>Sun Station</span>
+        </div>
+    </div>
+    <div id="programming_language">
+        <div id="programming_language_div_2">
+            <div id="programming_language_div_1"> 
+                <img id="php_programming" class="img_programming" src="/sunstation_write/server/material/php programming.png">
+                <img id="css_programming" class="img_programming" src="/sunstation_write/server/material/css programming.png">
+                <img id="js_programming" class="img_programming" src="/sunstation_write/server/material/js programming.png">
+                <img id="html_programming" class="img_programming" src="/sunstation_write/server/material/html programming.png">
+            </div>
+        </div>
+        <div id="programming_language_div_layer"></div>
+    </div>
+</footer>
